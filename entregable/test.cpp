bab2479e40506e309590a7f2da75cd5a39175b57
Damián Garro <dgarro@dc.uba.ar>
2018-04-07 17:08:39 -0300

    TABS

--- entregable/test.cpp
+++ entregable/test.cpp
@@ -3,20 +3,20 @@
 using namespace std;
 
 int main(void) {
-  Lista<int> lista;
-  lista.push_front(5);
-  cout<<lista.front()<<endl;
-  lista.push_front(4);
-  cout<<lista.front()<<endl;
-  lista.push_front(3);
-  cout<<lista.front()<<endl;
-  lista.push_front(2);
-  cout<<lista.front()<<endl;
-  lista.push_front(1);
-  cout<<lista.front()<<endl;
-  lista.push_front(0);
-  cout<<lista.front()<<endl;
-  for(int i =0; i<6; i++){
-    cout<<lista.iesimo(i)<<endl;
-  }
+	Lista<int> lista;
+	lista.push_front(5);
+	cout<<lista.front()<<endl;
+	lista.push_front(4);
+	cout<<lista.front()<<endl;
+	lista.push_front(3);
+	cout<<lista.front()<<endl;
+	lista.push_front(2);
+	cout<<lista.front()<<endl;
+	lista.push_front(1);
+	cout<<lista.front()<<endl;
+	lista.push_front(0);
+	cout<<lista.front()<<endl;
+	for(int i =0; i<6; i++){
+		cout<<lista.iesimo(i)<<endl;
+	}
 }
